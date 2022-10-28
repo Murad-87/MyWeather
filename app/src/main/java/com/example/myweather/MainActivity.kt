950ad9c9995c1737c0659d3437669bba8d4b5e97
Murad <m.iusupov>
2022-10-28 12:51:52 +0300

    test commit

--- app/src/main/java/com/example/myweather/MainActivity.kt
+++ app/src/main/java/com/example/myweather/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Test
         binding.btnGet.setOnClickListener {
             getResult("London")
         }
