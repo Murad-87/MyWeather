cfb16fe831a0d0624bad0ccda2522842beaa1b0e
Murad <m.iusupov>
2022-10-28 12:39:32 +0300

    start commit

---
+++ app/src/main/java/com/example/myweather/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.myweather
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.myweather.databinding.ActivityMainBinding
+import org.json.JSONObject
+
+const val API_KEY = "7609ba56694743bf9f5121111222710"
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnGet.setOnClickListener {
+            getResult("London")
+        }
+    }
+
+    private fun getResult(name: String) {
+        val url = "https://api.weatherapi.com/v1/current.json" +
+                "?key=$API_KEY&q=$name&aqi=no"
+        val queue = Volley.newRequestQueue(this)
+        val stringRequest = StringRequest(Request.Method.GET,
+            url,
+            {
+                    response ->
+                val obj = JSONObject(response)
+                val temp = obj.getJSONObject("current")
+                Log.d("MyLog", "Volley response: ${temp.getString("temp_c")}")
+            },
+            {
+                Log.d("MyLog", "Volley error: $it")
+            }
+            )
+        queue.add(stringRequest)
+    }
+}
